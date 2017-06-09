50d8a0ae213f7c5bfa80128faaf8529312767ba9
Lee Hinman <lee@writequit.org>
2017-06-09 13:41:10 -0600

    Beginning of multi-threading...

--- app/FanOut.hs
+++ app/FanOut.hs
@@ -1,11 +1,27 @@
 module Main where
 
 import Server
+import Control.Monad
+import Control.Concurrent (forkIO, threadDelay)
+import Control.Concurrent.MVar
+import Control.Concurrent.STM (STM(..), atomically)
+import Control.Concurrent.STM.TChan
+
+oneSecond :: Int
+oneSecond = 1000000
 
 main :: IO ()
 main = do
-  putStrLn $ "A server named: "
-    ++ (getName n)
-    ++ " with a queue size of "
-    ++ (show (getQueueSize n))
-  where n = newServer "foo"
+  chan <- atomically $ newTChan
+  continue <- newLatch
+  putStrLn "Starting readers.."
+  _ <- forkIO $ readAndExecute chan continue
+  _ <- forkIO $ readAndExecute chan continue
+  putStrLn "Starting writer.."
+  _ <- forkIO $ createOperations chan continue
+  putStrLn "Pausing"
+  threadDelay $ 2 * oneSecond
+  putStrLn "Exiting"
+  _ <- takeMVar continue
+  putMVar continue False
+  threadDelay oneSecond
--- src/Server.hs
+++ src/Server.hs
@@ -1,9 +1,13 @@
 module Server ( Server
               , newServer
               , getName
-              , getQueueSize) where
+              , readAndExecute
+              , newLatch
+              , createOperations) where
 
 import Operation as Op
+import Control.Monad
+import Control.Concurrent.MVar
 import Control.Concurrent.STM (STM(..), atomically)
 import Control.Concurrent.STM.TChan
 
@@ -20,9 +24,32 @@ newServer s = do
 getName :: Server -> String
 getName s = _name s
 
--- Execute next operation in the queue, returning a new Server with updated queue
-executeNextOp :: Server -> IO ()
-executeNextOp Server { _name=n, _queue=q } = do
-  atomically $ do
-    op <- readTChan q
-    Op.execute op
+-- -- Execute next operation in the queue, returning a new Server with updated queue
+-- executeNextOp :: Server -> IO ()
+-- executeNextOp Server { _name=n, _queue=q } = do
+--   atomically $ do
+--     op <- readTChan q
+--     liftM Op.execute op
+
+type Latch = MVar Bool
+
+newLatch :: IO (MVar Bool)
+newLatch = newMVar True
+
+readAndExecute :: TChan (Op.Operation Int) -> Latch -> IO ()
+readAndExecute chan latch = do
+  op <- atomically $ readTChan chan
+  putStrLn $ "got: " ++ show (Op.execute op)
+  continue <- readMVar latch
+  case continue of
+    True -> readAndExecute chan latch
+    False -> return ()
+
+createOperations :: TChan (Op.Operation Int) -> Latch -> IO ()
+createOperations chan latch =
+  let op = (Op.empty 1) in do
+    atomically $ writeTChan chan op
+    continue <- readMVar latch
+    case continue of
+      True -> createOperations chan latch
+      False -> return ()
